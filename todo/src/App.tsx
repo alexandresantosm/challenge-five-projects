736c2cd8d2f98fb6013e1a23b4838e9f9b115184
Alexandre Santos <amenezes031@gmail.com>
2021-10-13 20:19:06 -0300

    add ListItem component to the page

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,19 +1,15 @@
 import { Header } from "./components/Header";
+import { ListItem } from "./components/ListItem";
 import { Main } from "./components/Main";
 import { Wrapper } from "./components/Wrapper";
-import { useTodo } from "./hooks/UseTodo";
 
 const App = () => {
-  const { itens } = useTodo();
-
   return (
     <Wrapper>
       <Main>
         <Header title={'Lista de Tarefas'} />
 
-        {itens.map((item, index) => (
-          <div key={`item_${item.id}`}>{item.name}</div>
-        ))}
+        <ListItem />
 
       </Main>
     </Wrapper>
