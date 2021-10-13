7852483fb480f6ad108af74a3df0a7b333e2c5a5
Alexandre Santos <amenezes031@gmail.com>
2021-10-13 09:59:35 -0300

    add Main component to the page

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,9 +1,12 @@
+import { Main } from "./components/Main";
 import { Wrapper } from "./components/Wrapper";
 
 const App = () => {
   return (
     <Wrapper>
-      <h1>Hello World!</h1>
+      <Main>
+        <h1>Hello World!</h1>
+      </Main>
     </Wrapper>
   );
 }
