1f5c40a6686d43866d5876bafaf785a96c09ba1e
Alexandre Santos <amenezes031@gmail.com>
2021-10-13 17:25:49 -0300

    rendered item tasks

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,12 +1,20 @@
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Wrapper } from "./components/Wrapper";
+import { useTodo } from "./hooks/UseTodo";
 
 const App = () => {
+  const { itens } = useTodo();
+
   return (
     <Wrapper>
       <Main>
         <Header title={'Lista de Tarefas'} />
+
+        {itens.map((item, index) => (
+          <div key={`item_${item.id}`}>{item.name}</div>
+        ))}
+
       </Main>
     </Wrapper>
   );
