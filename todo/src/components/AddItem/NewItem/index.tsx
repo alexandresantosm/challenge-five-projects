2d34be2a528d578ff081ec3ade7fbaf63e3c915c
Alexandre Santos <amenezes031@gmail.com>
2021-10-14 20:41:11 -0300

    create NewItem component

---
+++ todo/src/components/AddItem/NewItem/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './style';
+
+export const NewItem = () => {
+    return (
+        <Container
+            type="text"
+            placeholder="Adicione uma tarefa"
+        />
+    );
+}
---
+++ todo/src/components/AddItem/NewItem/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.input`
+  flex: 1;
+  background-color: transparent;
+  border: 0;
+  outline: 0;
+  color: #fff;
+  font-size: 1.25rem;
+`;
