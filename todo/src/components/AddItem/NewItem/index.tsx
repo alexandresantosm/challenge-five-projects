16339867da2349a84ab631e42302260873a57841
Alexandre Santos <amenezes031@gmail.com>
2021-10-14 22:18:59 -0300

    add new item task in the task list

--- todo/src/components/AddItem/NewItem/index.tsx
+++ todo/src/components/AddItem/NewItem/index.tsx
@@ -1,10 +1,49 @@
+import { ChangeEvent, KeyboardEvent,useState } from 'react';
+import { useTodo } from '../../../hooks/UseTodo';
+
+import { createId } from '../../../utils/IdGenerator'
+
 import { Container } from './style';
 
 export const NewItem = () => {
+    const { items, addItem } = useTodo();
+    const initialState = '';
+    const [taskName, setTaskName] = useState(initialState);
+
+    const handleChangeTaskName = (event: ChangeEvent<HTMLInputElement>) => {
+        const newTask = event.target.value;
+
+        setTaskName(newTask);
+    }
+
+    const addNewItem = (event: KeyboardEvent) => {
+        const isNameValid = taskName !== '';
+        const isPressEnter = event.code === 'Enter';
+
+        if (isPressEnter && isNameValid) {
+            const id = createId(items);
+            const name = taskName;
+            const done = false;
+
+            const newItem = {
+                id,
+                name,
+                done
+            }
+
+            addItem(newItem);
+
+            setTaskName(initialState);
+        }
+    }
+
     return (
         <Container
             type="text"
             placeholder="Adicione uma tarefa"
+            value={taskName}
+            onChange={(event) => handleChangeTaskName(event)}
+            onKeyUp={(event) => addNewItem(event)}
         />
     );
 }
